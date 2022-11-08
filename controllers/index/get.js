171e3b1c58ac42f2263ac2a551712d1966e741c3
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2022-11-08 19:19:14 +0100

    -

--- controllers/index/get.js
+++ controllers/index/get.js
@@ -12,7 +12,7 @@ module.exports = (req, res) => {
       meta: {
         title: 'Scale the Future of Blockchain',
         description: 'Istanbul Blockchain Summit is a conference where solutions that will take blockchain technologies to the next level will be presented. This Summit will take place in the Spring of 2023. Participants from blockchain companies, developer communities, and community managers will evaluate solutions for the ecosystem\'s problems, such as sustainability, scalability, privacy, and financial volatility.',
-        image: '/res/images/open-graph/header.png'
+        // image: '/res/images/open-graph/header.png'
       }
     },
     end_time: END_TIME,
